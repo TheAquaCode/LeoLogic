02c7206465ef20f853549c2256a54faa7379b7d0
Nobody1989 <carsonshasteen1989@gmail.com>
2025-10-27 10:21:06 -0500

    setting position

--- src/components/tabs/Settings.jsx
+++ src/components/tabs/Settings.jsx
@@ -274,7 +274,7 @@ return (
   <div
     className="flex-1 flex flex-col overflow-hidden transition-all duration-500"
     style={{
-      width: chatbotMaximized ? 'calc(100% - 350px)' : '80%',
+      width: chatbotMaximized ? 'calc(100% - 300px)' : '100%',
       transition: 'width 0.3s ease, margin-right 0.3s ease',
     }}
   >
